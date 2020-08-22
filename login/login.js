368d4f5382335f1e7d2e0228a0910b10a7a489fc
Kwok <kwokrenx@gmail.com>
2020-08-22 14:33:25 -0400

    combining all javascript files together

--- login/login.js
+++ login/login.js
@@ -1,1 +1,42 @@
 console.log('Hello World')
+
+// where the heroku website will be
+// const deployedURL = ''
+const deployedURL = null
+// developmental purposes will use local host
+const URL = deployedURL ? deployedURL : "http://localhost:3000"
+
+
+const $createbutton = $('#createbutton')
+const $loginbutton = $('#loginbutton')
+
+$createbutton.on('click', async(event) => {
+    const create_info = {
+        username: $('#newusername').val(),
+        password: $('#newpassword').val()
+    }
+    const response = await fetch(`${URL}/users`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(create_info)
+    })
+    console.log(response);
+})
+
+$loginbutton.on('click', async(event) => {
+    const login_info = {
+        username: $('#username').val(),
+        password: $('#password').val()
+    }
+    const response = await fetch(`${URL}/login`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(login_info)
+    })
+    const r = await response.json()
+    console.log(r)
+})
