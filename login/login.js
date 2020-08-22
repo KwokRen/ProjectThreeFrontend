d219d440c86d9a1f7c0f4a4f23df9d569a1a685a
Kwok <kwokrenx@gmail.com>
2020-08-22 16:29:38 -0400

    merged all html pages other and starting vue

--- login/login.js
+++ login/login.js
@@ -1,45 +1,68 @@
-console.log('Hello World')
+// console.log('Hello World')
 
-// where the heroku website will be
-// const deployedURL = ''
-const deployedURL = null
-// developmental purposes will use local host
-const URL = deployedURL ? deployedURL : "http://localhost:3000"
+// // where the heroku website will be
+// // const deployedURL = ''
+// const deployedURL = null
+// // developmental purposes will use local host
+// const URL = deployedURL ? deployedURL : "http://localhost:3000"
 
 
-const $createbutton = $('#createbutton')
-const $loginbutton = $('#loginbutton')
-let token = null
-let user = null
+// const $createbutton = $('#createbutton')
+// const $loginbutton = $('#loginbutton')
+// let token = null
+// let user = null
 
-$createbutton.on('click', async(event) => {
-    const create_info = {
-        username: $('#newusername').val(),
-        password: $('#newpassword').val()
+// $createbutton.on('click', async(event) => {
+//     const create_info = {
+//         username: $('#newusername').val(),
+//         password: $('#newpassword').val()
+//     }
+//     const response = await fetch(`${URL}/users`, {
+//         method: "post",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(create_info)
+//     })
+//     console.log(response);
+// })
+
+// $loginbutton.on('click', async(event) => {
+//     const login_info = {
+//         username: $('#username').val(),
+//         password: $('#password').val()
+//     }
+//     const response = await fetch(`${URL}/login`, {
+//         method: "post",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(login_info)
+//     })
+//     const r = await response.json()
+//     token = r.token
+//     user = r.user.username
+// })
+
+// $('#submitcomment').on('click', () => {
+//     console.log(token);
+//     console.log(user);
+// })
+
+let app = new Vue ({
+    el: '#app',
+    data: {
+        loggedin: false,
+        JWT: "",
+        createUser: "",
+        createPass: "",
+        devURL: "http://localhost:3000",
+        prodURL: null,
+        user: null,
+        username: null,
+        token: null
+    },
+    methods: {
+        
     }
-    const response = await fetch(`${URL}/users`, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(create_info)
-    })
-    console.log(response);
-})
-
-$loginbutton.on('click', async(event) => {
-    const login_info = {
-        username: $('#username').val(),
-        password: $('#password').val()
-    }
-    const response = await fetch(`${URL}/login`, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(login_info)
-    })
-    const r = await response.json()
-    token = r.token
-    user = r.user.username
 })
