60cc1de2f7b90a510296d26c417e7237bb83ee6e
J-Morocho <jmorocho95@yahoo.com>
2020-08-22 23:25:41 -0400

    fetch data based on search query javascript

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+const API_KEY = 'AIzaSyDw5mk-09qwY2AFK06t0iE25JQqNHqxEiI'
+
+const getAllJavascriptVideos = async () => {
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=javascript&key=${API_KEY}`)
+    console.log(await response.json())
+}
+
+getAllJavascriptVideos()
