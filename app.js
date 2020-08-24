4672f54a6bd2f12dbd00b080f6ea827878832ec7
J-Morocho <jmorocho95@yahoo.com>
2020-08-23 22:10:07 -0400

    create function that populates database via fetch request

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
 const API_KEY = 'AIzaSyDw5mk-09qwY2AFK06t0iE25JQqNHqxEiI'
+const URL = "http://localhost:3000"
 
 const getAllJavascriptVideos = async () => {
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=javascript&key=${API_KEY}`)
-    console.log(await response.json())
-}
+    const fields = 'fields=items(id,snippet(title))'
+    const part = "part=id,snippet"
+    // returns an object {items : [ {id:{}, snippet:{}} ]}
+    let res = await fetch(`https://www.googleapis.com/youtube/v3/search?${part}&${fields}&maxResults=25&q=javascript&key=${API_KEY}`)
+    let arr = await res.json() // object
 
+    // define fetch function that creates videos on database
+    // Video model takes in title, like_count, dislike_count, videoID
+    let objArr = arr.items
+    objArr.forEach(element => {
+        let videoObj = { "title": element.snippet.title, "like_count": 0, "dislike_count": 0, "videoID": element.id.videoID}
+        fetch(`${URL}/videos`, {
+            method: 'post',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(videoObj)
+        })
+    });
+}
 getAllJavascriptVideos()
+
+// const test = new Vue({
+//     el: '#test',
+//     data: {
+//         items: null,
+//         message: 'hi'
+//     },
+//     methods: {
+//         getAllYoutubeVideos: function() {
+//             const fields = 'items(id,snippet(title))'
+//             const part = 'id,snippet'
+//             // returns array of video objects [ {id: {kind: string, videoId: string}, snippet{title: string}}) ]
+//             let data = fetch(`https://www.googleapis.com/youtube/v3/search?part=${part}&fields=${fields}&maxResults=25&q=javascript&key=${API_KEY}`)
+//                 .then(response => response.json())
+//                 .then(res => res)
+            
+//             data.array.forEach(element => {
+//                 console.log(element)
+//             });
+//         }
+//     },
+//     // Called right before mounting begins
+//     beforeMount(){
+//         // Should call a function that displays youtube videos
+//         this.getAllYoutubeVideos()
+//     }
+// })
+
+
+
