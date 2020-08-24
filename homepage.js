7b70154947a67fea94b097552ed57fc6f8b92c83
Kwok <kwokrenx@gmail.com>
2020-08-23 23:10:21 -0400

    added some styles, will change because not decided on color scheme

--- homepage.js
+++ homepage.js
@@ -2,3 +2,25 @@ $('.hamburgerdiv').on('click', () => {
     $('.hamburgerdiv').toggleClass('open')
     $('.hidden').toggleClass('show')
 })
+
+let app = new Vue ({
+    el: '#app',
+    data: {
+        displayvideo: false,
+        redbg: false,
+        greenbg: false,
+        displaycomment: false,
+        displaycomments: false
+    },
+    methods: {
+        displayVideo: function(event) {
+            this.displayvideo = true
+        },
+        displayHomepage: function(event) {
+            this.displayvideo = false
+        },
+        signUpToComment: function(event) {
+            alert("You must be logged in to comment")
+        }
+    }
+})
--- login/login.js
+++ login/login.js
@@ -1,54 +1,3 @@
-// console.log('Hello World')
-
-// // where the heroku website will be
-// // const deployedURL = ''
-// const deployedURL = null
-// // developmental purposes will use local host
-// const URL = deployedURL ? deployedURL : "http://localhost:3000"
-
-
-// const $createbutton = $('#createbutton')
-// const $loginbutton = $('#loginbutton')
-// let token = null
-// let user = null
-
-// $createbutton.on('click', async(event) => {
-//     const create_info = {
-//         username: $('#newusername').val(),
-//         password: $('#newpassword').val()
-//     }
-//     const response = await fetch(`${URL}/users`, {
-//         method: "post",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(create_info)
-//     })
-//     console.log(response);
-// })
-
-// $loginbutton.on('click', async(event) => {
-//     const login_info = {
-//         username: $('#username').val(),
-//         password: $('#password').val()
-//     }
-//     const response = await fetch(`${URL}/login`, {
-//         method: "post",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify(login_info)
-//     })
-//     const r = await response.json()
-//     token = r.token
-//     user = r.user.username
-// })
-
-// $('#submitcomment').on('click', () => {
-//     console.log(token);
-//     console.log(user);
-// })
-
 let app = new Vue ({
     el: '#app',
     data: {
@@ -121,3 +70,4 @@ let app = new Vue ({
         }
     }
 })
+
