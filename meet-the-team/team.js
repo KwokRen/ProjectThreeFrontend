81f4c9da39e1f2c8be1e6f555cae47b87d713a47
Kwok <kwokrenx@gmail.com>
2020-08-28 13:10:15 -0400

    cards done for meet the team page

---
+++ meet-the-team/team.js
@@ -0,0 +1,19 @@
+$('.container-jendri').on('click', () => {
+    $('.card-jendri').toggleClass('flip')
+})
+
+$('.container-rosmary').on('click', () => {
+    $('.card-rosmary').toggleClass('flip')
+})
+
+$('.container-shawn').on('click', () => {
+    $('.card-shawn').toggleClass('flip')
+})
+
+$('.container-caira').on('click', () => {
+    $('.card-caira').toggleClass('flip')
+})
+
+$('.container-kwok').on('click', () => {
+    $('.card-kwok').toggleClass('flip')
+})
